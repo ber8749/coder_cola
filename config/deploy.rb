d8eaeefbf1f0385edcf56e7fae5ef8cde9f038fc
ber8749 <ber8749@gmail.com>
2016-01-14 19:01:23 -0800

    Try setting rbenv_path as solution to bundler issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,9 @@ set :application, 'coder_cola'
 set :repo_url, 'git@github.com:ber8749/coder_cola.git'
 set :deploy_user, 'deploy'
 
-#set :rbenv_type, :user # or :system, depends on your rbenv setup
-#set :rbenv_ruby, '2.2.3'
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.2.3'
+set :rbenv_path, '$HOME/.rbenv'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
