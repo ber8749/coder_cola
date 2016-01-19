dd69193a64f8e4aa5f3ecf48d6dfb4370d48a3be
ber8749 <ber8749@gmail.com>
2016-01-18 23:32:06 -0800

    views:orders: add min attribute to quantity field. Add admin address to seeds. Update readme.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 Product.create(name: 'Coder Cola Case', description: '24 cans of Coder Cola', price: 14.99)
-User.create(first_name: 'Coder', last_name: 'Cola', role: 'admin', email: 'codercola@example.com', password: 'WXYz1234')
+user = User.create(first_name: 'Coder', last_name: 'Cola', role: 'admin', email: 'codercola@example.com', password: 'WXYz1234')
+user.addresses.create(line1: '1 Fake St.', line2: 'Apt. Z', city: 'San Diego', state: 'California', postal_code: '92024', country: 'United States', kind: 'shipping')
